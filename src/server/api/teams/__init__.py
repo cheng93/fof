575ef8af15df650d4f632a21e2aeac641c73275f
cheng93 <derek.c@hotmail.co.uk>
2018-06-30 15:16:04 +0100

    get_team

--- src/server/api/teams/__init__.py
+++ src/server/api/teams/__init__.py
@@ -6,21 +6,22 @@ from api.teams.get_teams import GetTeamsCommand
 
 routes = web.RouteTableDef()
 
-@routes.view('/teams/')
+@routes.view("/teams/")
 class TeamsView(web.View, CommandRunnerMixin):
     async def get(self):
         command = GetTeamsCommand(self.request.app["db"])
         data = await self.execute(command)
-        json = simplejson.dumps({'teams': data})
+        json = simplejson.dumps({"teams": data})
         return web.json_response(text=json)
 
 
 
-@routes.view('/teams/{team_id}')
+@routes.view("/teams/{team_id}")
 class TeamView(web.View, CommandRunnerMixin):
     async def get(self):
         db = self.request.app["db"]
-        team_id = self.request.match_info['team_id']
+        team_id = self.request.match_info["team_id"]
         command = GetTeamCommand(db, team_id)
-        data = await self.execute(command)
-        return web.json_response(data)
+        (team, seasons) = await self.execute(command)
+        json = simplejson.dumps({**team, "seasons": seasons})
+        return web.json_response(text=json)
--- src/server/api/teams/get_team.py
+++ src/server/api/teams/get_team.py
@@ -1,11 +1,22 @@
+import sqlalchemy as sa
 from marshmallow import Schema, fields
 from api.command import Command
-from db import team
+from db import division, team
+from db.views import (team_final_standing, team_season_summary, 
+                        team_overall_summary)
+
+class GetTeamSeasonSchema(Schema):
+    class Meta:
+        fields = ("team_id", "year", "wins", "loses", "ties",
+                    "win_lose_percent", "standing_name")
 
 class GetTeamSchema(Schema):
     name = fields.Function(lambda obj: f"{obj.city} {obj.team_name}")
     class Meta:
-        fields = ("team_id", "name")
+        fields = ("team_id", "name", "wins", "loses", "ties",
+                    "win_lose_percent", "conference_wins",
+                    "superbowl_wins", "playoff_appearances",
+                    "division")
 
 class GetTeamCommand(Command):
     def __init__(self, db, team_id):
@@ -14,9 +25,45 @@ class GetTeamCommand(Command):
 
     async def execute(self):
         async with self.db.acquire() as conn:
-            query = team.select().where(team.c.team_id == self.team_id)
-            cursor = await conn.execute(query)
+            cursor = await conn.execute(self.seasons_query)
+            records = await cursor.fetchall()
+            schema = GetTeamSeasonSchema()
+            (seasons, errors) = schema.dump(records, many=True)
+
+            cursor = await conn.execute(self.team_query)
             record = await cursor.first()
             schema = GetTeamSchema()
-            (data, errors) = schema.dump(record)
-            return data
+            (t, errors) = schema.dump(record)
+
+            return (t, seasons)
+
+    @property
+    def seasons_query(self):
+        join = (team_season_summary
+            .join(team_final_standing,
+                sa.and_(
+                    team_final_standing.c.team_id == team_season_summary.c.team_id,
+                    team_final_standing.c.year == team_season_summary.c.year)))
+        return (sa.select([
+                    team_season_summary,
+                    team_final_standing.c.standing_name
+                ])
+                .select_from(join)
+                .where(team_season_summary.c.team_id == self.team_id)
+                .order_by(team_season_summary.c.year))
+
+    @property
+    def team_query(self):
+        join = (team
+            .join(team_overall_summary,
+                team_overall_summary.c.team_id == team.c.team_id)
+            .join(division,
+                division.c.division_id == team.c.division_id))
+        return (sa.select([
+                    team_overall_summary,
+                    team.c.team_name,
+                    team.c.city,
+                    division.c.name.label("division")
+                ])
+                .select_from(join)
+                .where(team.c.team_id == self.team_id))
