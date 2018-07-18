0c3fcb64646993d16241452eb305ba9174110ff2
cheng93 <derek.c@hotmail.co.uk>
2018-07-18 21:13:58 +0100

    tabs component

---
+++ src/client/app/components/tabs/tabs.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav class="mdc-tab-bar">
+            <span v-for="(header, index) in headers"
+                :key="index"
+                v-on:click="selectIndex(index)"
+                v-bind:class="{'mdc-tab--active': selectedIndex == index}"
+                class="mdc-tab">
+                {{header}}
+            </span>
+        </nav>
+        <div v-for="(header, index) in headers"
+            :key="index"
+            v-if="selectedIndex == index">
+            <slot :name="'tab-' + index"></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selectedIndex: 0
+        };
+    },
+    methods: {
+        selectIndex: function(index) {
+            this.selectedIndex = index;
+        }
+    },
+    props: {
+        headers: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
